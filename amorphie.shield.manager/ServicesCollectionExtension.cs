54b798ce6d50608bbe2a3ba9fbee8e835b9ec374
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-06-27 10:54:43 +0300

    dev purpose

--- amorphie.shield.manager/ServicesCollectionExtension.cs
+++ amorphie.shield.manager/ServicesCollectionExtension.cs
@@ -26,6 +26,13 @@ public static class ServicesCollectionExtension
         var vaultOptions = new VaultOptions();
         builder.Configuration.GetSection(VaultOptions.Vault).Bind(vaultOptions);
         vaultOptions.RoleName = builder.Configuration[VaultOptions.ROLE_NAME] ?? throw new KeyNotFoundException("Vault role name expected");
+        //TODO: Remove these 5 lines
+        var vaultTokenFileName = builder.Configuration["Vault:TokenFileName"] ?? throw new KeyNotFoundException("Vault token file name expected");// "admin";//Environment.GetEnvironmentVariable("VAULT_TOKEN");
+        var vaultTokenFormFile = File.ReadAllText(vaultTokenFileName) ?? throw new KeyNotFoundException("Vault token expected");
+        Console.WriteLine("Vault token file path: " + vaultTokenFileName);
+        Console.WriteLine("Vault token : " + vaultTokenFormFile);
+        Console.WriteLine("Vault role name : " + vaultOptions.RoleName);
+        
         builder.Services.AddScoped(sp => vaultOptions);
     }
     public static VaultClient VaultClientCreator(ConfigurationManager configuration)
@@ -33,6 +40,7 @@ public static class ServicesCollectionExtension
         var vaultAddress = configuration[VaultOptions.VAULT_ADDR]; //; "http://127.0.0.1:8200";
         var vaultTokenFileName = configuration["Vault:TokenFileName"] ?? throw new KeyNotFoundException("Vault token file name expected");// "admin";//Environment.GetEnvironmentVariable("VAULT_TOKEN");
         var vaultTokenFormFile = File.ReadAllText(vaultTokenFileName) ?? throw new KeyNotFoundException("Vault token expected");
+        Console.WriteLine("Vault token : " + vaultTokenFormFile);
         var authMethod = new TokenAuthMethodInfo(vaultTokenFormFile);
         var vaultClientSettings = new VaultClientSettings(vaultAddress, authMethod);
         return new VaultClient(vaultClientSettings);
